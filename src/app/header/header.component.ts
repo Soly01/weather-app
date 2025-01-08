dd86b630aa8add8ab03cccc1d3087d046978e03c
Soly01 <solimana425@yahoo.com>
2025-01-09 00:18:11 +0200

    making header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
